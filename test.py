3cfa67451b3db5018ec1ed149d3cd31172ff2576
hithesh2201 <hithesh2201@gmail.com>
2024-11-27 16:00:05 +0530

    arrays&ll

---
+++ test.py
@@ -0,0 +1,64 @@
+class Node:
+    """A Node of a linked list."""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    """A Linked List."""
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        """Add a node to the end of the linked list."""
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def prepend(self, data):
+        """Add a node to the beginning of the linked list."""
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, data):
+        """Delete the first node with the given data."""
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next and current.next.data != data:
+            current = current.next
+        if current.next:
+            current.next = current.next.next
+
+    def display(self):
+        """Print the linked list."""
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+# Example usage:
+linked_list = LinkedList()
+linked_list.append(10)
+linked_list.append(20)
+linked_list.append(30)
+linked_list.prepend(5)
+
+print("Linked List after adding elements:")
+linked_list.display()
+
+linked_list.delete(20)
+print("Linked List after deleting 20:")
+linked_list.display()
